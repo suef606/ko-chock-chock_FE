5ae438c2b4367e6ac6a1556bb77975090d008235
suef606 <sufe3005@gmail.com>
2025-02-19 12:12:09 +0900

    feat: enable reactStrictMode and experimental options
    - Enabled  for stricter React checks
    - Added

--- next.config.mjs
+++ next.config.mjs
@@ -1,20 +1,9 @@
-// /** @type {import('next').NextConfig} */
-// const nextConfig = {
-//   // experimental: {
-//   //   appDir: true, // app 디렉토리 활성화
-//   // },
-//   eslint: {
-//     ignoreDuringBuilds: true,
-//   },
-//   images: {
-//     domains: ["ko-chock-chock.s3.ap-northeast-2.amazonaws.com"],
-//   },
-// };
-
-// export default nextConfig;
-
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  reactStrictMode: true,
+  experimental: {
+    missingSuspenseWithCSRBailout: false,
+  },
   eslint: {
     ignoreDuringBuilds: true,
   },
